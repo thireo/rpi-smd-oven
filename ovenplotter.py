e4b231f8b551de9075a18656d0a45fbe7aa4a4cf
Andreas <dk.andreas@gmail.com>
2019-02-17 19:20:26 +0100

    Now parses -f as filename

--- ovenplotter.py
+++ ovenplotter.py
@@ -3,43 +3,58 @@ import csv
 from datetime import datetime
 import time
 import numpy
+import argparse
+import sys
 
 
 
-timestamps = []
-thermos = []
-diffs = []
-timestampFormat = '%H:%M:%S.%f'
-i = 0
+def main(args):
+    parser = argparse.ArgumentParser(description="Plots temperature readings from given '-f LOGFILE.log' file.")
+    parser.add_argument("-f", "--filename", default= '', required=False)
+    args = parser.parse_args(args)
+    plot(args.filename)
 
-with open('oven-testing.log','r') as file:
-    reader = csv.reader(file,delimiter='\t')
-    for timestamp,thermo,internal in reader:
-        temp = thermo.split('t')
-        temp2 = temp[1].split('*C')
-        thermos.append(float(temp2[0]))
+def plot(filename):
 
-        if(i==0):
-            t1 = datetime.strptime(timestamp,timestampFormat)
-            startOfTime = t1.hour*60*60+t1.minute*60+t1.second+t1.microsecond/1e6
-            print(startOfTime)
-            timestamps.append(0)
-            i = 1
-        else:
-            t1 = datetime.strptime(timestamp,timestampFormat)
-            elapsedTime = t1.hour*60*60+t1.minute*60+t1.second+t1.microsecond/1e6-startOfTime
-            timestamps.append(elapsedTime)
-            
-diffs = numpy.diff(thermos)
+    timestamps = []
+    thermos = []
+    diffs = []
+    timestampFormat = '%H:%M:%S.%f'
+    i = 0
+    print('Loading: ',filename,'...')
+    #with open('oven-testing.log','r') as file:
+    with open(filename,'r') as file:
+        reader = csv.reader(file,delimiter='\t')
+        for timestamp,thermo,internal in reader:
+            temp = thermo.split('t')
+            temp2 = temp[1].split('*C')
+            thermos.append(float(temp2[0]))
+
+            if(i==0):
+                t1 = datetime.strptime(timestamp,timestampFormat)
+                startOfTime = t1.hour*60*60+t1.minute*60+t1.second+t1.microsecond/1e6
+                print(startOfTime)
+                timestamps.append(0)
+                i = 1
+            else:
+                t1 = datetime.strptime(timestamp,timestampFormat)
+                elapsedTime = t1.hour*60*60+t1.minute*60+t1.second+t1.microsecond/1e6-startOfTime
+                timestamps.append(elapsedTime)
+                
+    diffs = numpy.diff(thermos)
 
 
-plt.subplot(2,1,1)
-plt.plot(timestamps,thermos)
-plt.ylabel('Temperatur [C]')
-plt.xlabel('Tid [s]')
-plt.grid()
-plt.subplot(2,1,2)
+    plt.subplot(2,1,1)
+    plt.plot(timestamps,thermos)
+    plt.ylabel('Temperatur [C]')
+    plt.xlabel('Tid [s]')
+    plt.grid()
+    plt.subplot(2,1,2)
 
-plt.plot(diffs)
-plt.grid()
-plt.show()
+    plt.plot(diffs)
+    plt.grid()
+    plt.show()
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
